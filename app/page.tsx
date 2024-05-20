4c2b83d24dc2a0adb00d7496db3af249e9277c78
Oskar Michalkiewicz <oskar.michalkiewicz@gmail.com>
2024-05-20 21:44:16 +0200

    landing page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,30 @@
-import { CubeTransparentIcon } from "@heroicons/react/24/outline";
-import clsx from "clsx";
+import {
+  ArrowRightIcon,
+  CubeTransparentIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { Button } from "~/components/button";
 
 export default async function HomePage() {
   return (
-    <main className="m-auto w-[800px] ">
-      <div className={`flex flex-row items-center gap-1 leading-none`}>
-        <CubeTransparentIcon className="h-60 rotate-[30deg]" />
-        <p className="text-[10rem]">Gallery</p>
+    <main className="absolute inset-0 m-auto h-fit w-fit space-y-2.5">
+      <div className="flex h-auto justify-center rounded-lg bg-violet-900 p-3">
+        <div className={`flex flex-row items-center gap-1 leading-none`}>
+          <CubeTransparentIcon className="h-28 rotate-[30deg]" />
+          <p className="text-[5rem]">Gallery</p>
+        </div>
       </div>
-
-      <div className="m-auto max-w-36">
-        <Link
-          href="/login"
-          className={clsx(
-            "mb-2 flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-600 p-3 text-sm font-medium text-violet-200 hover:bg-slate-600 hover:text-violet-200 md:flex-none md:justify-start md:p-2 md:px-3",
-          )}
-        >
-          <p className="text-lg">Login</p>
+      <div className="flex w-full gap-4 rounded-md bg-slate-600 p-4">
+        <Link href="/login" className="mt-4 w-full">
+          <Button className="w-full">
+            Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-slate-50" />
+          </Button>
         </Link>
-        <Link
-          href="/register"
-          className={clsx(
-            "flex h-[48px] grow items-center justify-center rounded-md bg-slate-600 p-3 text-sm font-medium text-violet-200 hover:bg-slate-600 hover:text-violet-200 md:flex-none md:justify-start md:p-2 md:px-3",
-          )}
-        >
-          <p className="text-lg">Register</p>
+        <Link href="/register" className="mt-4 w-full">
+          <Button className="w-full">
+            Register
+            <ArrowRightIcon className="ml-auto h-5 w-5 text-slate-50" />
+          </Button>
         </Link>
       </div>
     </main>
